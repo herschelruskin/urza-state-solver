326b576b8074e79067d66ab710c521badf5b138b
herschelruskin <herschelruskin@gmail.com>
2026-08-22 09:26:14 -0400

    Guard shuffle epoch out of profiler denominator

--- strategic_value_state_smoke.py
+++ strategic_value_state_smoke.py
@@ -43,11 +43,7 @@ def key(state, info=None, **kwargs):
 
 def test_hidden_order_and_seed_do_not_fragment_value_identity():
     a = base_state(rng_root_seed=1)
-    b = replace(
-        a,
-        library=("Swan Song", "Island", "Mox Opal", "Sol Ring"),
-        rng_root_seed=999,
-    )
+    b = replace(a, library=("Swan Song", "Island", "Mox Opal", "Sol Ring"), rng_root_seed=999)
     assert canonical_markov_state_key(a) != canonical_markov_state_key(b)
     assert key(a) == key(b)
 
@@ -64,10 +60,9 @@ def test_reporting_history_and_legacy_flags_do_not_fragment_base_value():
         top_access=not a.top_access,
     )
     assert key(a) == key(b)
-
-    wa = replace(a, won=True, win_family="Chrome Dome")
-    wb = replace(a, won=True, win_family="Top + Reality Chip")
-    assert key(wa) == key(wb)
+    assert key(replace(a, won=True, win_family="Chrome Dome")) == key(
+        replace(a, won=True, win_family="Top + Reality Chip")
+    )
 
 
 def test_information_constraints_change_belief_identity():
@@ -76,7 +71,6 @@ def test_information_constraints_change_belief_identity():
     top_known = InformationState(known_top=("Island",))
     other_top_known = InformationState(known_top=("Swan Song",))
     counted = InformationState(known_library_counts=(("Island", 1),))
-
     assert key(s, no_knowledge) != key(s, top_known)
     assert key(s, top_known) != key(s, other_top_known)
     assert key(s, no_knowledge) != key(s, counted)
@@ -96,32 +90,17 @@ def test_real_resources_and_pending_state_remain_distinct():
     assert key(s) != key(replace(s, remora_upkeep_pending=True))
     assert key(s) != key(replace(s, saga3_pending=True))
     assert key(s) != key(replace(s, commander_casts_from_zone=1))
-
     untapped = replace(s, battlefield=(Perm("Island", tapped=False), Perm("Mox Opal")))
     assert key(s) != key(untapped)
 
 
 def test_perm_provenance_is_ignored_but_refund_credit_is_not():
-    common = dict(
-        name="Grinding Station",
-        tapped=True,
-        sick=False,
-        knack_granted=True,
-    )
-    a = base_state(
-        battlefield=(Perm(**common, knack_source="Banishing Knack", instance_tag=1),)
-    )
-    b = base_state(
-        battlefield=(Perm(**common, knack_source="Retraction Helix", instance_tag=999),)
-    )
+    common = dict(name="Grinding Station", tapped=True, sick=False, knack_granted=True)
+    a = base_state(battlefield=(Perm(**common, knack_source="Banishing Knack", instance_tag=1),))
+    b = base_state(battlefield=(Perm(**common, knack_source="Retraction Helix", instance_tag=999),))
     assert key(a) == key(b)
-
-    credited = base_state(
-        battlefield=(Perm(**common, producer_urza_ready=True),)
-    )
-    no_credit = base_state(
-        battlefield=(Perm(**common, producer_urza_ready=False),)
-    )
+    credited = base_state(battlefield=(Perm(**common, producer_urza_ready=True),))
+    no_credit = base_state(battlefield=(Perm(**common, producer_urza_ready=False),))
     assert key(credited) != key(no_credit)
 
 
@@ -145,19 +124,12 @@ def test_library_belief_uses_multiset_not_order():
 
 def test_profiler_measures_collapse_without_changing_states():
     a = base_state(rng_root_seed=1)
-    b = replace(
-        a,
-        library=("Mox Opal", "Swan Song", "Sol Ring", "Island"),
-        rng_root_seed=2,
-    )
-    before_a = a
-    before_b = b
-
+    b = replace(a, library=("Mox Opal", "Swan Song", "Sol Ring", "Island"), rng_root_seed=2)
+    before_a, before_b = a, b
     profiler = StrategicKeyProfiler()
     profiler.observe(a, InformationState())
     profiler.observe(b, InformationState())
     summary = profiler.summary()
-
     assert a == before_a and b == before_b
     assert summary["observations"] == 2
     assert summary["concrete_unique"] == 2
@@ -175,12 +147,22 @@ def test_profiler_keeps_same_concrete_state_with_different_information_distinct(
     profiler.observe(state, InformationState())
     profiler.observe(state, InformationState(known_top=("Island",)))
     summary = profiler.summary()
-
     assert summary["observations"] == 2
     assert summary["concrete_unique"] == 1
     assert summary["concrete_information_unique"] == 2
     assert summary["strategic_unique"] == 2
     assert summary["concrete_information_to_strategic_collapse_fraction"] == 0.0
+
+
+def test_profiler_denominator_ignores_shuffle_epoch_only():
+    state = base_state()
+    profiler = StrategicKeyProfiler()
+    profiler.observe(state, InformationState(shuffle_epoch=1))
+    profiler.observe(state, InformationState(shuffle_epoch=99))
+    summary = profiler.summary()
+    assert summary["concrete_unique"] == 1
+    assert summary["concrete_information_unique"] == 1
+    assert summary["strategic_unique"] == 1
 
 
 def test_projection_is_a_separate_value_object_not_policy_view():
@@ -205,6 +187,7 @@ def main():
         test_library_belief_uses_multiset_not_order,
         test_profiler_measures_collapse_without_changing_states,
         test_profiler_keeps_same_concrete_state_with_different_information_distinct,
+        test_profiler_denominator_ignores_shuffle_epoch_only,
         test_projection_is_a_separate_value_object_not_policy_view,
     ]
     for test in tests:
